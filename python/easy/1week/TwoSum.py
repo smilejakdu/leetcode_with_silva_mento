9fd18ed295b28744d0db71cb2d769e67f928c750
smilejakdu <ash982416@gmail.com>
2020-07-27 09:53:00 +0900

    python_algo

---
+++ python/easy/1week/TwoSum.py
@@ -0,0 +1,36 @@
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for n1 in range(0 , len(nums)):
+            for n2 in range(1 , len(nums)):
+                if nums[n1]+nums[n2] == target and n1 != n2:
+                    return [n1,n2]
+
+'''
+Other solution
+'''
+def twoSum(self, nums, target):
+        seen = {}
+        for i, v in enumerate(nums):
+            remaining = target - v
+            if remaining in seen:
+                return [seen[remaining], i]
+            seen[v] = i
+        return []
