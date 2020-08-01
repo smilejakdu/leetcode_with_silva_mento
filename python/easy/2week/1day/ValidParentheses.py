59c4221f27e2d1e52c69bf2fe25b5fa61f8e9a90
smilejakdu <ash982416@gmail.com>
2020-08-01 09:58:27 +0900

    2week_start

--- python/easy/2week/1day/ValidParentheses.py
+++ python/easy/2week/1day/ValidParentheses.py
@@ -30,19 +30,23 @@ Output: true
 
 '''
 
-Input = "()[]{}"
-
-string_diction = {
-    "{": "}",
-    "[": "]",
-    "(": ")"
-}
-
+Input = "()"
 
 class Solution(object):
     def isValid(self, string):
-        for k,v in string_diction.items():
-            print(k,v)
+
+        left = ['(', '{', '[']
+        right = [')', '}', ']']
+        stack = []
+        for letter in string:
+            if letter in left:
+                stack.append(letter)
+            elif letter in right:
+                if len(stack) <= 0:
+                    return False
+                if left.index(stack.pop()) != right.index(letter):
+                    return False
+        return len(stack) == 0
 
 
 s = Solution()
