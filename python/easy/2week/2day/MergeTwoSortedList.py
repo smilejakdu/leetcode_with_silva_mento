a22d6e1682ac376463fdd1d8bf5bcddbe0871f27
smilejakdu <ash982416@gmail.com>
2020-08-03 15:02:08 +0900

    2week_first

---
+++ python/easy/2week/2day/MergeTwoSortedList.py
@@ -0,0 +1,26 @@
+''':arg
+Merge two sorted linked lists and return it as a new sorted list.
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+
+잘 모르겠음 .. ㅠㅠ
+'''
+
+list1 = [1, 2, 4]
+list2 = [1, 3, 4]
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        result = []
+        result.extend(l1)
+        result.extend(l2)
+
+        return sorted(result)
+
+s = Solution()
+print(s.mergeTwoLists(list1, list2))
