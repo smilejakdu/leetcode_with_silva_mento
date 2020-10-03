ce5af450b4454e115ef951f1004596a577167765
smilejakdu <ash982416@gmail.com>
2020-10-03 18:26:19 +0900

    jupyter_주식_method2

--- 프로그레머스/Level2/주식가격.py
+++ 프로그레머스/Level2/주식가격.py
@@ -37,4 +37,20 @@ def solution(prices):
 
 print(solution(prices))
 
+# 두번째 방법
+prices = [1 , 2 , 3 , 2 , 3]
+
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+
+            if prices[i] >prices[j]:
+                break
+            else:
+                answer[i] +=1
+    return answer
+
+print(solution(prices))
 
